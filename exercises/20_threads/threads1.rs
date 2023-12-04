a5b3aee2cda8b74515c4226b42a0e02b3a57539e
leonchan <leonchantinglok@gmail.com>
2023-12-04 08:44:17 +0800

    20

--- exercises/20_threads/threads1.rs
+++ exercises/20_threads/threads1.rs
@@ -8,8 +8,6 @@
 // Execute `rustlings hint threads1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::thread;
 use std::time::{Duration, Instant};
 
@@ -20,13 +18,13 @@ fn main() {
             let start = Instant::now();
             thread::sleep(Duration::from_millis(250));
             println!("thread {} is complete", i);
-            start.elapsed().as_millis()
+            start.elapsed().as_millis() //This line returns u128, so it is 128
         }));
     }
 
     let mut results: Vec<u128> = vec![];
     for handle in handles {
-        // TODO: a struct is returned from thread::spawn, can you use it?
+        results.push(handle.join().unwrap());
     }
 
     if results.len() != 10 {
